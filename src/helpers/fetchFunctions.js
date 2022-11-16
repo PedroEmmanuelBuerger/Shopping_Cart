2e747d8ec6a3f951682fd425d532cd829d11f10c
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-11-16 15:44:38 -0300

    requesito 1 e 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,12 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (parametro) => {
+  if (!parametro) { throw new Error('Termo de busca não informado')}
+  const apiSearch = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${parametro}`)
+  try {
+    const result = await apiSearch.json();
+    return result.results
+  }
+  catch (erro) { return erro.message }
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -4,16 +4,20 @@ import computadorSearch from './mocks/search';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
-  it('fetchProductsList é uma função', () => {
-    
+  it('fetchProductsList é uma função', async () => {
+    expect(typeof fetchProductsList).toBe('function')
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    const promiseAwaiting = fetch('')
+    expect(fetchProductsList('computador')).toEqual(promiseAwaiting)
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const computadores = (await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador'))
+    const array = await computadores.json();
+    const result = await array.results
+    expect(await fetchProductsList('computador')).toEqual(result)
   });
 
   // it('...', () => {
