f37fe1f7ec1952a3960566178bbb52052b7f6d7a
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-11-16 17:43:30 -0300

    requesito 3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const lista = await fetchProductsList('computador')
+const ids = lista.map(Element => Element.id)
+const titles = lista.map(Element => Element.title)
+const imgs = lista.map(Element => Element.thumbnail)
+const prices = lista.map(Element => Element.price)
+const localSection = document.querySelector('.products')
+for (let i = 0; i <lista.length; i += 1 ) {
+    const obj = {
+        id: ids[i],
+        title: titles[i],
+        thumbnail: imgs[i],
+        price: prices[i]
+        }
+localSection.appendChild(createProductElement(obj)) 
+}
