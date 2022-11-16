9b371aad3700880a1e50219a07434edc550066c6
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-11-16 19:52:42 -0300

    requesito 4,5 e 6

--- src/main.js
+++ src/main.js
@@ -5,18 +5,28 @@ import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-const lista = await fetchProductsList('computador')
-const ids = lista.map(Element => Element.id)
-const titles = lista.map(Element => Element.title)
-const imgs = lista.map(Element => Element.thumbnail)
-const prices = lista.map(Element => Element.price)
 const localSection = document.querySelector('.products')
-for (let i = 0; i <lista.length; i += 1 ) {
-    const obj = {
-        id: ids[i],
-        title: titles[i],
-        thumbnail: imgs[i],
-        price: prices[i]
-        }
-localSection.appendChild(createProductElement(obj)) 
+const loadingText = document.createElement('h1');
+loadingText.setAttribute('class', 'loading');
+loadingText.innerText = 'Carregando...'
+localSection.appendChild(loadingText)
+// 
+const lista = await fetchProductsList('computador')
+try{
+    loadingText.remove(localSection)
+    const ids = lista.map(Element => Element.id)
+    const titles = lista.map(Element => Element.title)
+    const imgs = lista.map(Element => Element.thumbnail)
+    const prices = lista.map(Element => Element.price)
+    
+    for (let i = 0; i <lista.length; i += 1 ) {
+        const obj = {
+            id: ids[i],
+            title: titles[i],
+            thumbnail: imgs[i],
+            price: prices[i]
+            }
+    localSection.appendChild(createProductElement(obj)) 
+    }
 }
+catch(erro) {console.log('Algum erro ocorreu, recarregue a página e tente novamente')}
